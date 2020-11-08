5c41c31f5b515e0cd3d1250a317c7ab798b40c92
superpuper32 <pochitalin_m@mail.ru>
2020-11-08 12:01:01 +0300

    brain even logic implemented

--- src/even-logic.js
+++ src/even-logic.js
@@ -2,16 +2,47 @@ import promptly from 'promptly';
 
 const { log } = console;
 const { prompt } = promptly;
+const LIMIT = 100;
+
+function randomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
+
+const checkEven = (num) => num % 2 === 0;
+
+const checkAnswer = (answer, even) => {
+  switch (answer) {
+    case 'yes':
+      return !!even;
+    case 'no':
+      return !even;
+    default:
+      return false;
+  }
+};
 
 export default async () => {
   log('Welcome to the Brain Games!');
-
   const name = await prompt('May I have your name? ');
+  let count = 0;
 
-  log(`Hello, ${name}`);
+  log(`Hello, ${name}!`);
   log('Answer "yes" if the number is even, otherwise answer "no".');
 
-  log('Question: number ');
-  const answer = await prompt('Your answer: ');
-  log(answer);
+  while (count < 3) {
+    const number = randomInt(LIMIT);
+    log(`Question: ${number}`);
+    const answer = await prompt('Your answer: '); // 'yes' ?? 'no'
+    const even = checkEven(number); // true ?? false
+    const right = checkAnswer(answer, even);
+    if (right) {
+      log('Correct!');
+      count += 1;
+    } else {
+      log(`Let's try again, ${name}!`);
+      count = 0;
+    }
+  }
+
+  log(`Congratulations, ${name}!`);
 };
