fe4c319f26def61783acef0554e5fb3e3da6a7bf
superpuper32 <pochitalin_m@mail.ru>
2020-11-20 01:01:47 +0300

    refactored calc for new realities

--- bin/brain-calc.js
+++ bin/brain-calc.js
@@ -1,4 +1,4 @@
 #!/usr/bin/env node
-import { startBrainCalc } from '../src/index.js'
+import startBrainCalc from '../src/games/calc.js'
 
 startBrainCalc()
--- src/games/calc.js
+++ src/games/calc.js
@@ -1,14 +1,13 @@
-import welcomeGreetings, {
+import { cons } from '@hexlet/pairs'
+
+import startGame from '../index.js'
+import {
   randomInt,
-  promptQuestion,
-  promptAnswer,
-  incorrectLog,
-  finishGame,
 } from '../cli.js'
 
 const MIN = 1
 const LIMIT = 100
-const { log } = console
+const clause = 'What is the result of the expression?'
 
 const expressions = {
   '+': (a, b) => a + b,
@@ -17,36 +16,22 @@ const expressions = {
 }
 
 const randomSign = (operators) => {
-  const random = randomInt(1, operators.length - 1)
+  const random = randomInt(MIN, operators.length - 1)
 
   return operators[random]
 }
 
 const createQuestion = (a, b, sign) => `${a} ${sign} ${b}`
 
-export default async () => {
-  const name = await welcomeGreetings()
-  let rounds = 0
+const generateData = () => {
+  const a = randomInt(MIN, LIMIT)
+  const b = randomInt(MIN, LIMIT)
+  const sign = randomSign(Object.keys(expressions))
 
-  log('What is the result of the expression?')
+  const question = createQuestion(a, b, sign)
+  const correctAnswer = expressions[sign](a, b).toString()
 
-  while (rounds < 3) {
-    const a = randomInt(MIN, LIMIT)
-    const b = randomInt(MIN, LIMIT)
-    const sign = randomSign(Object.keys(expressions))
-    const question = createQuestion(a, b, sign)
-    promptQuestion(question)
-    const answer = await promptAnswer()
-
-    const correctAnswer = expressions[sign](a, b).toString()
-    if (answer === correctAnswer) {
-      log('Correct!');
-      rounds += 1
-    } else {
-      incorrectLog(answer, correctAnswer, name)
-      rounds = 0
-    }
-  }
-
-  finishGame(name)
+  return cons(question, correctAnswer)
 }
+
+export default () => startGame(clause, generateData)
