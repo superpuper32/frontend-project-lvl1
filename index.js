186e999afedbc283d49802c4e5c048e6ec5cdccf
superpuper32 <pochitalin_m@mail.ru>
2020-12-07 22:06:01 +0300

    export startBrainGames added

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 /* eslint-disable import/no-named-as-default */
 /* eslint-disable import/no-named-as-default-member */
-/* eslint-disable no-unused-vars */
 import startBrainGames from './bin/brain-games.js';
+
+export default startBrainGames;
